670641b551fa62bcb04c5e93eca6863b3999fbff
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-07 14:46:24 +0200

    Correct example

--- src/heritage/example.js
+++ src/heritage/example.js
@@ -16,6 +16,23 @@ class TvShow {
 }
 */
 
+// L’héritage permet, lors de la déclaration d’une nouvelle classe ou d’un nouvel objet, d’y inclure les caractéristiques (telles que les propriétés et méthodes) d’une autre classe.
+// La classe Media ci-dessus contiendra la fonctionnalité pour lancer le film ou la série via la méthode play
+
+class Media {
+    constructor(url) {
+        this._url = url
+    }
+
+    play() {
+        /**
+         * Ici, je ne lance pas le film, je fais simplement
+         * un console.log mais le résultat est le même ;)
+         */
+        console.log(this._url)
+    }
+}
+
 // Adaptation des classes de bases pour intégrer l'héritage de Media
 // L'utilisation du mot clé extends permet aux classes Movie et TvShow de récupérer la fonctionnalité play
 // super() permet de passer les paramètres de l'URL à la classe Media
@@ -38,29 +55,19 @@ class Movie extends Media {
     }
  }
 
-// Ici j'instancie quelques classes de films
-const PredatorMovie = new Movie("Predator", 1987, 107)
-const TerminatorMovie = new Movie("Terminator", 1984, 107)
-const AlienMovie = new Movie("Alien", 1979, 117)
+// // Ici j'instancie quelques classes de films
+// const PredatorMovie = new Movie("Predator", 1987, 107)
+// const TerminatorMovie = new Movie("Terminator", 1984, 107)
+// const AlienMovie = new Movie("Alien", 1979, 117)
 
-// Ici j'instancie quelques séries
-const FriendsTvShow = new TvShow("Friends", 23, 10)
-const ScrubsTvShow = new TvShow("Scrubs", 20, 9)
-const CougarTownTvShow = new TvShow("Cougar Town", 13, 5)
+// // Ici j'instancie quelques séries
+// const FriendsTvShow = new TvShow("Friends", 23, 10)
+// const ScrubsTvShow = new TvShow("Scrubs", 20, 9)
+// const CougarTownTvShow = new TvShow("Cougar Town", 13, 5)
 
-// L’héritage permet, lors de la déclaration d’une nouvelle classe ou d’un nouvel objet, d’y inclure les caractéristiques (telles que les propriétés et méthodes) d’une autre classe.
-// La classe Media ci-dessus contiendra la fonctionnalité pour lancer le film ou la série via la méthode play
 
-class Media {
-    constructor(url) {
-        this._url = url
-    }
+// Je crée mon instance pour le film Predator et je renseigne bien l'URL
+const PredatorMovie = new Movie("https//www.google.com", "Predator", 1987, 107)
 
-    play() {
-        /**
-         * Ici, je ne lance pas le film, je fais simplement
-         * un console.log mais le résultat est le même ;)
-         */
-        console.log(this._url)
-    }
-}
+// Je lance Predator via la méthode play de la classe Media
+PredatorMovie.play()
