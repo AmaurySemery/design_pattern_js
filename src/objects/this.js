0aaa23f1e264fb0f80c9c0cfee3ddec832f1ed22
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-07 14:29:37 +0200

    Add

---
+++ src/objects/this.js
@@ -0,0 +1,28 @@
+/*
+Dans le snippet de code qui suit,  this  correspond au contexte d'exécution de la classe. 
+Autrement dit, c’est ce qui me permet de récupérer l’attribut  firstName  dans la méthode  getFullName. 
+Si vous oubliez ce mot clé, vous ne serez pas capable de communiquer avec les méthodes et propriétés composant votre classe.
+*/
+
+class Me {
+    constructor(firstName, lastName) {
+        this._firstName = firstName
+        this._lastName = lastName
+    }
+ 
+    getFullName() {
+        return `${this._firstName} ${this._lastName}`
+    }
+ 
+    sayHello() {
+        console.log("Hello")
+    }
+ }
+ 
+ const Thomas = new Me("Thomas", "Dimnet")
+ 
+ // Retourne mon prénom et mon nom
+ Thomas.getFullName()
+ 
+ // Dit bonjour
+ Thomas.sayHello()
