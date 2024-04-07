d8f809b94fa7183e82abe1f4af8024069eb920ee
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-07 15:25:38 +0200

    Correction de l'exercice sur les constructor patterns

--- src/exampleProject/js/App.js
+++ src/exampleProject/js/App.js
@@ -1,17 +1,23 @@
 class App {
     constructor() {
         this.$moviesWrapper = document.querySelector('.movies-wrapper')
-        this.moviesApi = new MovieApi('/data/old-movie-data.json')
+        
+        this.moviesApi = new MovieApi('/data/new-movie-data.json')
+        this.externalMoviesApi = new MovieApi('/data/external-movie-data.json')
     }
 
     async main() {
-        // Ici je récupère mes films de mon fichier old-movie-data.json
-        const moviesData = await this.moviesApi.getMovies()
-        
-        moviesData
-            // Ici, je transforme mon tableau de données en un tableau de classe Movie
-            .map(movie => new OldMovie(movie))
-            .forEach(movie => {
+        const moviesData = await this.moviesApi.get()
+        const externalMoviesData = await this.externalMoviesApi.get()
+
+        const Movies = moviesData.map(movie => new MoviesFactory(movie, 'newApi'))
+        const ExternalMovies = externalMoviesData.map(movie => new MoviesFactory(movie, 'externalApi'))
+
+        console.log(Movies)
+
+        const FullMovies = Movies.concat(ExternalMovies)
+
+        FullMovies.forEach(movie => {
                 const Template = new MovieCard(movie)
                 this.$moviesWrapper.appendChild(
                     Template.createMovieCard()
