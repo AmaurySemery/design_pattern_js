4b461fa3c4c0822a3b6c59e793e224a2b1708357
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-07 15:18:29 +0200

    Comment constructor pattern

--- src/designPatterns/creationalDesignPattern/constructorPattern.js
+++ src/designPatterns/creationalDesignPattern/constructorPattern.js
@@ -1,3 +1,9 @@
+/*
+Un Constructor Pattern est souvent utilisé pour faciliter la création et/ou le formatage d’objets. Il sert pour :
+Formater des données. Ces données peuvent provenir d’une API, d’un Local Storage ou d’un Session Storage.
+Créer des objets.
+*/
+
 // Le Parent
 class Movie {
     constructor(data) {
