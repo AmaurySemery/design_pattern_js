1aabbfd5dc086c0296b93409453473de473f8c9e
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-07 14:39:40 +0200

    Update heritage

--- src/heritage/example.js
+++ src/heritage/example.js
@@ -1,25 +1,64 @@
+/*
 class Movie {
     constructor(title, releasedDate, duration) {
         this._title = title
         this._releasedDate = releasedDate
         this._duration = duration
     }
- }
- 
- class TvShow {
+}
+
+class TvShow {
     constructor(title, numberOfEpisodePerSeason, numberOfSeasons) {
         this._title = title
         this._numberOfEpisodePerSeason = numberOfEpisodePerSeason
         this._numberOfSeasons = numberOfSeasons
     }
+}
+*/
+
+// Adaptation des classes de bases pour intégrer l'héritage de Media
+
+class Movie extends Media {
+    constructor(url, title, releasedDate, duration) {
+        super(url)
+        this._title = title
+        this._releasedDate = releasedDate
+        this._duration = duration
+    }
  }
  
- // Ici j'instancie quelques classes de films
- const PredatorMovie = new Movie("Predator", 1987, 107)
- const TerminatorMovie = new Movie("Terminator", 1984, 107)
- const AlienMovie = new Movie("Alien", 1979, 117)
- 
- // Ici j'instancie quelques séries
- const FriendsTvShow = new TvShow("Friends", 23, 10)
- const ScrubsTvShow = new TvShow("Scrubs", 20, 9)
- const CougarTownTvShow = new TvShow("Cougar Town", 13, 5)
+ class TvShow extends Media {
+    constructor(url, title, numberOfEpisodePerSeason, numberOfSeasons) {
+        super(url)
+        this._title = title
+        this._numberOfEpisodePerSeason = numberOfEpisodePerSeason
+        this._numberOfSeasons = numberOfSeasons
+    }
+ }
+
+// Ici j'instancie quelques classes de films
+const PredatorMovie = new Movie("Predator", 1987, 107)
+const TerminatorMovie = new Movie("Terminator", 1984, 107)
+const AlienMovie = new Movie("Alien", 1979, 117)
+
+// Ici j'instancie quelques séries
+const FriendsTvShow = new TvShow("Friends", 23, 10)
+const ScrubsTvShow = new TvShow("Scrubs", 20, 9)
+const CougarTownTvShow = new TvShow("Cougar Town", 13, 5)
+
+// L’héritage permet, lors de la déclaration d’une nouvelle classe ou d’un nouvel objet, d’y inclure les caractéristiques (telles que les propriétés et méthodes) d’une autre classe.
+// La classe Media ci-dessus contiendra la fonctionnalité pour lancer le film ou la série via la méthode play
+
+class Media {
+    constructor(url) {
+        this._url = url
+    }
+
+    play() {
+        /**
+         * Ici, je ne lance pas le film, je fais simplement
+         * un console.log mais le résultat est le même ;)
+         */
+        console.log(this._url)
+    }
+}
