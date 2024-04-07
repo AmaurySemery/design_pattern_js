fce64ca9f29032b615d9442d2ea8e9ac19a2518b
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-07 15:23:38 +0200

    Mise à jour en conséquence de app.js

--- src/exampleProject/js/App.js
+++ src/exampleProject/js/App.js
@@ -1,16 +1,22 @@
 class App {
     constructor() {
         this.$moviesWrapper = document.querySelector('.movies-wrapper')
-        this.moviesApi = new MovieApi('/data/movie-data.json')
+        this.moviesApi = new MovieApi('/data/old-movie-data.json')
     }
 
     async main() {
-        const movies = await this.moviesApi.getMovies()
-
-        movies.forEach(movie => {
-            const Template = new MovieCard(movie)
-            this.$moviesWrapper.appendChild(Template.createMovieCard())        
-        })    
+        // Ici je récupère mes films de mon fichier old-movie-data.json
+        const moviesData = await this.moviesApi.getMovies()
+        
+        moviesData
+            // Ici, je transforme mon tableau de données en un tableau de classe Movie
+            .map(movie => new OldMovie(movie))
+            .forEach(movie => {
+                const Template = new MovieCard(movie)
+                this.$moviesWrapper.appendChild(
+                    Template.createMovieCard()
+                )
+            })
     }
 }
 
