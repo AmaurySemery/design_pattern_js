2be34cddda5af89cd82c73a5f7472d2352c25030
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-08 15:58:05 +0200

    Commentaires pour les adapter pattern

--- src/designPatterns/structuralDesignPattern/adapterPattern.js
+++ src/designPatterns/structuralDesignPattern/adapterPattern.js
@@ -1,3 +1,15 @@
+/*
+Un Adapter Pattern est composé de trois acteurs :
+
+* Le client qui correspond à l’objet (ou plus généralement à l’élément) qui réalise la requête. Dans le cas de notre projet fil rouge, le client est notre objet   FilterForm  .
+* L’adapter est l’objet qui va être utilisé par le client. Le client va instancier l’adapter, et c’est l’adapter qui sera chargé de faire les modifications pour pouvoir communiquer avec l’adapté.
+* L’adapté qui va être appelé par l’adapter. Ici, c’est bien l’adapter qui se charge de faire la transposition entre l’ancienne librairie et la nouvelle.
+*/
+
+// Dès que vous réalisez une mise à jour vers une nouvelle API, vous pouvez passer par un Adapter.
+// De cette manière, le code est modifié avec une solution propre sans avoir à réécrire une grosse partie.
+// Il suffit  d'ajouter un nouvel objet qui sera chargé de faire la traduction entre deux objets aux interfaces incompatibles.
+
 class OldCalculator {
     constructor() {
         this.operations = function(number1, number2, operation) {
