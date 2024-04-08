880ccc5383ad0c9c5eb7022283acc6c44207aedc
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-08 16:21:08 +0200

    Commentaires sur le Proxy Pattern

--- src/designPatterns/structuralDesignPattern/proxyPattern.js
+++ src/designPatterns/structuralDesignPattern/proxyPattern.js
@@ -1,3 +1,21 @@
+/*
+Un Proxy Pattern est composé des acteurs suivants :
+
+* Le Client : c’est la fonction ou l’objet qui va appeler le proxy.
+* Le Proxy qui va être l’objet chargé de faire la communication entre le client et le Sujet.
+* Le Sujet qui va être l’objet appelé par le proxy. 
+*/
+
+// Un proxy est un composant logiciel qui joue le rôle d’intermédiaire entre deux hôtes pour faciliter ou surveiller leurs échanges. Un hôte peut être un objet (comme c’est le cas dans notre projet), mais cela peut aussi être un serveur informatique.
+// Dès lors qu’un objet est un peu complexe à manipuler, nous pouvons mettre en place un proxy pour faciliter sa manipulation.
+
+/*
+L’avantage d’utiliser un Proxy Pattern pour la gestion du cache, c’est que nous allons avoir des gains de performance très importants :
+
+* Lors de la première requête auprès de notre librairie, le temps de réponse sera le même (1 seconde), sauf que nous allons stocker le résultat de la fonction dans notre proxy.
+* Lors de la deuxième requête, le proxy va regarder si le résultat de la fonction a été stocké (ce qui sera le cas). Il va alors retourner directement le résultat plutôt que de rappeler à nouveau la fonction de la librairie. Le temps de réponse sera cette fois instantané !
+*/
+
 class GeoCoder {
     static ADDRESSES = [
         {
