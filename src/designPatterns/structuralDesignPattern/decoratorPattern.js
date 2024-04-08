7792fffb029ede72cd7965305638cd701cee9f40
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-08 16:09:26 +0200

    Commentaires sur le Decorator Pattern

--- src/designPatterns/structuralDesignPattern/decoratorPattern.js
+++ src/designPatterns/structuralDesignPattern/decoratorPattern.js
@@ -1,3 +1,16 @@
+// Un Decorator Pattern permet d'ajouter une fonctionnalité à un objet
+
+/*
+Un Decorator Pattern est composé de trois acteurs :
+
+* Le premier est leClient. C’est, ici, l’objet ou la fonction qui va appeler le Decorator. Dans le cas du projet, le client va être l’objetApp.
+* Le deuxième est le Component. C’est l’objet sans la nouvelle fonctionnalité. Pour Filmo Patterns, le component estMovieCard. Ici, cet objet ne sait pas qu’il est “décoré” (on pourrait aussi dire qu’il est surchargé).
+* Le troisième acteur est leDecorator. Cet objet va récupérer un objet existant, le surcharger (en lui ajoutant une nouvelle fonctionnalité), et va le retourner.
+*/
+
+// Permet de rapidement ajouter de nouvelles fonctionnalités à un objet sans avoir à le surcharger ou à le complexifier
+// Antippatern : surcharger un decorator qui surcharge un decorator et ainsi de suite
+
 class Movie {
     constructor(title, duration) {
         this._title = title
@@ -28,3 +41,5 @@ class Movie {
  
  // J'instancie un objet Movie décoré
  const MovieWithAgeLimit = movieWithAgeLimit(new Movie('Alien', 120), 16)
+
+ // movieWithAgeLimit permet rapidement d'ajouter de nouvelles fonctionnalités à un objet existant
