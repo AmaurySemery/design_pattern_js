44a67262e4ae706a12a0c06c3ebf09c7438dbae7
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-09 16:36:01 +0200

    Commentaires sur l'Observer Pattern

--- src/designPatterns/behavioralDesignPattern/observerPattern.js
+++ src/designPatterns/behavioralDesignPattern/observerPattern.js
@@ -1,3 +1,14 @@
+// L’Observer Pattern crée un objet qui va notifier d’autres objets de votre code au moment de changements d’état.
+
+/*
+Un Observer Pattern est composé des acteurs suivants :
+* Vous avez le Sujet. Ce dernier maintient la liste des observateurs. Il permet à des observateurs de s’abonner ou de se désabonner. Une fois que l’état du sujet change, grâce à ce système d’abonnement, les observateurs vont être notifiés que l’état du sujet a changé.
+* Puis, vous avez le ou les Observers. Lorsqu’ils vont être notifiés d’un changement d’état du sujet, ils vont pouvoir déclencher un script. Ici, il est important de voir qu’il peut y avoir plusieurs observateurs différents pour un même sujet.
+*/
+
+// Quand on parle d’Observers, on dit souvent qu’ils permettent de “découpler” une architecture 
+// Plutôt que de faire grossir un objet en lui ajoutant une nouvelle fonctionnalité, en utilisant un Decorator par exemple, on va plutôt créer des objets qui vont communiquer entre eux : on aurait donc plein de “petits” objets qui feront des tâches spécifiques.
+
 class Subject {
     constructor() {
         this._observers = []
