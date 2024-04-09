1d9009528bcccabec3fcd98d30f27f2deee5a791
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-09 16:52:15 +0200

    Commentaires pour le State Pattern

--- src/designPatterns/behavioralDesignPattern/statePattern.js
+++ src/designPatterns/behavioralDesignPattern/statePattern.js
@@ -1,3 +1,15 @@
+/*
+Un State Pattern est composé des éléments suivants :
+* Vous avez le Context. Cet objet va vous permettre d’une part de connaître le State courant, et d’autre part d’effectuer les modifications de State. 
+* Vous avez le ou les States. Ce sont des objets qui composent l’objet Context.
+*/
+
+// Le principe d’un State Pattern est de gérer facilement les changements d’état d’une application
+
+// Chaque State correspond à un des états possibles de l’application. Par exemple, pour un utilisateur, on pourrait avoir   connected ,   anonymous , etc.
+
+// Le State Pattern est souvent bien pratique pour le débogage. Vous connaissez par avance dans quel état se trouve votre application, et vous centralisez vos changements d’état à un seul endroit.
+
 // Le Context
 class TrafficLight {
     constructor() {
